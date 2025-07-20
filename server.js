bfc8efd7fafc7601989e07700d39bd4bb1d8c0bd
emmanuel <emmanueludofot@gmail.com>
2025-07-20 13:00:34 +0100

    1

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-require('dotenv').config();
+require('dotenv').config({ 
+  path: process.env.NODE_ENV === 'production' 
+    ? '.env.production' 
+    : '.env.development'
+});
 const app = require('./app');
 const mongoose = require('mongoose');
 const { setupWebSocket } = require('./utils/socketManager');
